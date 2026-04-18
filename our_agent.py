fe2d2727529acef3dbbbce805ab9049cff983dfb
CatherineDelvaux <catherinedelvaux@yahoo.fr>
2026-04-18 21:03:59 +0200

    basic alpha beta

--- our_agent.py
+++ our_agent.py
@@ -7,8 +7,7 @@ class OurAgent(Agent):
         super().__init__(player)
     
     def act(self, state, remaining_time):
-        actions = list(Game.actions(state))
-        return random.choice(actions)
+        return self.AlphaBetaSearch(state, remaining_time)
     
     def evaluate(self, state):
         our_score = 0
@@ -33,10 +32,10 @@ class OurAgent(Agent):
 
     def score_group(self, group, player):
             our_color_count = 0
-            our_symbol_count = 0
-
             opponent_color_count = 0
-            opponent_symbol_count = 0
+
+            x_count = 0
+            o_count = 0
 
             for i in group:
                 if i != None:
@@ -44,6 +43,11 @@ class OurAgent(Agent):
                         our_color_count += 1
                     else:
                         opponent_color_count += 1
+
+                    if i[0]=="x":
+                        x_count+=1
+                    if i[0]=="o":
+                        o_count+=1
 
             color_score = 0
             if opponent_color_count == 0:
@@ -53,5 +57,60 @@ class OurAgent(Agent):
                     color_score = 10
                 if our_color_count == 3:
                     color_score = 100
+
+            symbol_score = 0
+            best_symbol = max(x_count, o_count)
+            if best_symbol == 1:
+                symbol_score = 1
+            if best_symbol == 2:
+                symbol_score = 10
+            if best_symbol == 3:
+                symbol_score = 100
             
-            return color_score
+
+            return color_score + symbol_score
+    
+    #algorithme vu en cours
+    def AlphaBetaSearch(self, state, remaining_time):
+        v, action = self.max_value(state, float('-inf'), float('inf'), 3)
+        return action
+    
+    def max_value(self, state, alpha, beta, depth):
+        if Game.is_terminal(state):
+            return Game.utility(state, self.player), None
+        if depth == 0:
+            return self.evaluate(state), None
+        
+        v = float('-inf')
+        action = None
+        
+        for a in Game.actions(state):
+            new_state = state.copy()
+            Game.apply(new_state, a)
+            v2, _ = self.min_value(new_state, alpha, beta, depth-1)
+            if v2 > v:
+                v, action = v2, a
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, action
+        return v, action
+    
+    def min_value(self, state, alpha, beta, depth):
+        if Game.is_terminal(state):
+            return Game.utility(state, self.player), None
+        if depth == 0:
+            return self.evaluate(state), None
+        
+        v = float('inf')
+        action = None
+        
+        for a in Game.actions(state):
+            new_state = state.copy()
+            Game.apply(new_state, a)
+            v2, _ = self.max_value(new_state, alpha, beta, depth-1)
+            if v2 < v:
+                v, action = v2, a
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, action
+        return v, action
